d89120ecf2ff272dffe0b8d925f50ba6fae60d2c
Hugo Nijhuis-Mekkelholt <hugo@senzible.tech>
2023-08-11 10:46:29 +0200

    moved runtime to its own package, made signals types

---
+++ wasm/internal/reactive/runtime.go
@@ -0,0 +1,115 @@
+package reactive
+
+type Runtime struct {
+	//slice of signal values
+	signalValues      []any
+	runningEffect     *EffectId
+	signalSubscribers map[SignalId][]EffectId
+	effects           []func()
+}
+
+func NewRuntime() *Runtime {
+	return &Runtime{
+		signalValues:      []any{},
+		signalSubscribers: map[SignalId][]EffectId{},
+		effects:           []func(){},
+	}
+}
+
+func (rt *Runtime) CreateSignal(initial any) Signal {
+	id := SignalId(len(rt.signalValues))
+	rt.signalValues = append(rt.signalValues, initial)
+	return Signal{rt, id}
+}
+
+func CreateSignal[T any](rt *Runtime, initial T) TypedSignal[T] {
+	id := SignalId(len(rt.signalValues))
+	rt.signalValues = append(rt.signalValues, initial)
+	return TypedSignal[T]{rt, id}
+}
+
+func (rt *Runtime) CreateEffect(f func()) {
+	effectId := EffectId(len(rt.effects))
+	rt.effects = append(rt.effects, f)
+
+	rt.runEffect(effectId)
+}
+
+func (rt *Runtime) runEffect(id EffectId) {
+	//keep track of previous running effect
+	previous := rt.runningEffect
+
+	//set the current running effect
+	rt.runningEffect = &id
+
+	//run the effect
+	rt.effects[id]()
+
+	//reset the running effect
+	rt.runningEffect = previous
+}
+
+type SignalId int
+type EffectId int
+
+type Signal struct {
+	*Runtime
+	SignalId
+}
+
+type TypedSignal[T any] struct {
+	*Runtime
+	SignalId
+}
+
+func (s *TypedSignal[T]) Get() T {
+	//get the value
+	val := s.Runtime.signalValues[s.SignalId]
+
+	//if there is a running effect add it to subscribers, only if it's not already subscribed
+	if rt := s.Runtime; rt.runningEffect != nil {
+		if _, ok := rt.signalSubscribers[s.SignalId]; !ok {
+			rt.signalSubscribers[s.SignalId] = append(rt.signalSubscribers[s.SignalId], *rt.runningEffect)
+		}
+	}
+
+	//return value
+	return val.(T)
+}
+
+func (s *TypedSignal[T]) Set(v T) {
+	//set the value
+	s.Runtime.signalValues[s.SignalId] = v
+
+	//notify subscribers
+	for _, effectId := range s.Runtime.signalSubscribers[s.SignalId] {
+		s.Runtime.runEffect(effectId)
+	}
+}
+
+func (s *Signal) Get() any {
+	//get the value
+	val := s.Runtime.signalValues[s.SignalId]
+
+	//if there is a running effect, add it to the subscribers
+	//only if it is not already subscribed
+	if rt := s.Runtime; rt.runningEffect != nil {
+		if _, ok := rt.signalSubscribers[s.SignalId]; !ok {
+			//append the effect to the subscribers
+			rt.signalSubscribers[s.SignalId] = append(rt.signalSubscribers[s.SignalId], *rt.runningEffect)
+		}
+	}
+
+	//return value
+	return val
+}
+
+func (s *Signal) Set(v any) {
+	//set the value
+	s.Runtime.signalValues[s.SignalId] = v
+
+	//notify subscribers
+	for _, effectId := range s.Runtime.signalSubscribers[s.SignalId] {
+		s.Runtime.runEffect(effectId)
+	}
+}
--- wasm/main.go
+++ wasm/main.go
@@ -3,77 +3,9 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+
+	"github.com/senzible/drago/wasm/internal/reactive"
 )
-
-type Runtime struct {
-	//slice of signal values
-	signalValues      []any
-	runningEffect     *EffectId
-	signalSubscribers map[SignalId][]EffectId
-	effects           []func()
-}
-
-func (rt *Runtime) CreateSignal(initial any) Signal {
-	id := SignalId(len(rt.signalValues))
-	rt.signalValues = append(rt.signalValues, initial)
-	return Signal{rt, id}
-}
-
-func (rt *Runtime) CreateEffect(f func()) {
-	effectId := EffectId(len(rt.effects))
-	rt.effects = append(rt.effects, f)
-
-	rt.RunEffect(effectId)
-}
-
-func (rt *Runtime) RunEffect(id EffectId) {
-	//keep track of previous running effect
-	previous := rt.runningEffect
-
-	//set the current running effect
-	rt.runningEffect = &id
-
-	//run the effect
-	rt.effects[id]()
-
-	//reset the running effect
-	rt.runningEffect = previous
-}
-
-type SignalId int
-type EffectId int
-
-type Signal struct {
-	*Runtime
-	SignalId
-}
-
-func (s *Signal) Get() any {
-	//get the value
-	val := s.Runtime.signalValues[s.SignalId]
-
-	//if there is a running effect, add it to the subscribers
-	//only if it is not already subscribed
-	if rt := s.Runtime; rt.runningEffect != nil {
-		if _, ok := rt.signalSubscribers[s.SignalId]; !ok {
-			//append the effect to the subscribers
-			rt.signalSubscribers[s.SignalId] = append(rt.signalSubscribers[s.SignalId], *rt.runningEffect)
-		}
-	}
-
-	//return value
-	return val
-}
-
-func (s *Signal) Set(v any) {
-	//set the value
-	s.Runtime.signalValues[s.SignalId] = v
-
-	//notify subscribers
-	for _, effectId := range s.Runtime.signalSubscribers[s.SignalId] {
-		s.Runtime.RunEffect(effectId)
-	}
-}
 
 func main() {
 	c := make(chan struct{})
@@ -94,12 +26,8 @@ func main() {
 	body.Call("appendChild", p)
 	body.Call("appendChild", dec)
 
-	rt := Runtime{
-		signalValues:      []any{},
-		signalSubscribers: map[SignalId][]EffectId{},
-		effects:           []func(){},
-	}
-	count := rt.CreateSignal(0)
+	rt := reactive.NewRuntime()
+	count := reactive.CreateSignal(rt, 0)
 
 	rt.CreateEffect(func() {
 		p.Set("textContent", fmt.Sprintf("Count: %d", count.Get()))
@@ -107,12 +35,12 @@ func main() {
 
 	//add event listeners
 	inc.Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		count.Set(count.Get().(int) + 1)
+		count.Set(count.Get() + 1)
 		return nil
 	}))
 
 	dec.Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		count.Set(count.Get().(int) - 1)
+		count.Set(count.Get() - 1)
 		return nil
 	}))
 
