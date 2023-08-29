708373234bf430a88185850e4718809702573cb4
Hugo Nijhuis-Mekkelholt <hugo@senzible.tech>
2023-08-29 17:36:41 +0200

    added senzible.design.wasm

---
+++ senzible.design.wasm/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/senzible/drago/wasm/element"
+	"github.com/senzible/drago/wasm/reactive"
+)
+
+func main() {
+	c := make(chan struct{})
+
+	element.MountFunc(func(rt *reactive.Runtime) element.Element {
+
+		return element.NewElement("div").
+			Child(
+				element.NewElement("img").Attr("src", "/logo-design.svg"),
+			)
+	})
+
+	<-c
+}
